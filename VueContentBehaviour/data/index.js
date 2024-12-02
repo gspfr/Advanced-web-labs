b645b32bbfac5313d43d85c11122895ac62eec74
gspfr <124562294+gspfr@users.noreply.github.com>
2024-12-02 19:59:44 -0400

    Vue project (part1 ?)

---
+++ VueContentBehaviour/data/index.js
@@ -0,0 +1,23 @@
+export const items = [
+  {
+    title: "Football",
+    description:
+      "A sport played by two teams of eleven player with a goal on each end of the field. The ball is played by kicking it and to win you have to score more goals than the other team.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/France_champion_of_the_Football_World_Cup_Russia_2018.jpg/420px-France_champion_of_the_Football_World_Cup_Russia_2018.jpg",
+  },
+  {
+    title: "Judo",
+    description:
+      "A martial art created in Japan in 1882 by Jigoro Kano. It is a sport that demands physical and mental discipline. It requires strength, balance, coordination, technique and flexibility.",
+    image:
+      "https://images.bfmtv.com/pFfX0x4XLJhOzFbCFyKgR8-OpO8=/0x0:2048x1365/580x0/images/L-equipe-de-France-de-judo-apres-avoir-conquis-l-or-par-equipe-JO-de-Paris-2024-3-aout-2024-1914495.webp",
+  },
+  {
+    title: "Workout",
+    description:
+      "A physical activity that can be done in a gym, at home or even outside. It is a great way to keep your body healthy and in shape.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUogZX1-Kyz-kieESfPcLnXHEogd1LxYcygg&s",
+  },
+];
