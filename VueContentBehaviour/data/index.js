d784160a7e8406c5335e16c2846d1bfabf7cc189
gspfr <124562294+gspfr@users.noreply.github.com>
2024-12-03 11:39:49 -0400

    Add files via upload

--- VueContentBehaviour/data/index.js
+++ VueContentBehaviour/data/index.js
@@ -20,4 +20,11 @@ export const items = [
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUogZX1-Kyz-kieESfPcLnXHEogd1LxYcygg&s",
   },
+  {
+    title: "Running",
+    description:
+      "Running is excellent for your health and for your other sports. It is fondamental even though it is hard to start and stay motivated.",
+    image:
+      "https://hips.hearstapps.com/hmg-prod/images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1036780592-1553033495.jpg?resize=640:*",
+  },
 ];
