f40e31022c26e1c334c62fdc88ee7f5eefa42457
gspfr <124562294+gspfr@users.noreply.github.com>
2024-12-17 22:35:21 -0400

    Add files via upload

---
+++ VueWebsite/data/index.js
@@ -0,0 +1,122 @@
+export const items = [
+  {
+    name: "Airpods",
+    description: "Bluetooth headphones",
+    price: 100,
+    url: {
+      white:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MRXJ2?wid=1144&hei=1144&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1551489688000",
+    },
+  },
+  {
+    name: "Bluetooth speaker",
+    description: "Portable Bluetooth Speaker",
+    price: 29.99,
+    url: {
+      black: "https://m.media-amazon.com/images/I/81p9DPknjLL._AC_SX466_.jpg",
+      blue: "https://m.media-amazon.com/images/I/81zbuotvZkL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+    },
+  },
+  {
+    name: "Subway line",
+    description: "TTC Line 1, 2 or 4 for an attractive price. Don't hesitate !",
+    price: 10,
+    url: {
+      yellow:
+        "https://upload.wikimedia.org/wikipedia/commons/2/23/TTC_-_Line_1_-_Yonge-University-Spadina_line.svg",
+      green:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/TTC_-_Line_2_-_Bloor-Danforth_line.svg/741px-TTC_-_Line_2_-_Bloor-Danforth_line.svg.png",
+      purple:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/TTC_-_Line_4_-_Sheppard_line.svg/741px-TTC_-_Line_4_-_Sheppard_line.svg.png",
+    },
+  },
+  {
+    name: "Wireless Gaming Controller",
+    description:
+      "MSI Force Wireless Gaming Controller Compatible with PC & Android",
+    price: 39.99,
+    url: {
+      white: "https://m.media-amazon.com/images/I/61jlgdLGBRL._SX522_.jpg",
+    },
+  },
+  {
+    name: "Big Arch Burger",
+    description: "The new McDonald's burger is here !",
+    price: 7.99,
+    url: {
+      white:
+        "https://s7d1.scene7.com/is/image/mcdonalds/mcdonalds-big-arch-1:product-header-desktop?wid=829&hei=455&dpr=off",
+      black:
+        "https://e7.pngegg.com/pngimages/584/242/png-clipart-black-hamburger-on-brown-tray-burger-king-black-bunned-halloween-whopper-food-hamburgers.png",
+    },
+  },
+  {
+    name: "Macbook Pro 2020",
+    description: "Apple Macbook Pro 2020",
+    price: 1299.99,
+    url: {
+      white:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202011?wid=892&hei=820&&qlt=80&.v=1603406905000",
+    },
+  },
+  {
+    name: "Apple Watch Series 6",
+    description: "Apple Watch Series 6",
+    price: 399.99,
+    url: {
+      black:
+      "https://as-images.apple.com/is/MTHT3_AV2?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1693090914854",
+      red:
+      "https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-series-6-aluminum-red-case_09152020_carousel.jpg.large.jpg",
+      white:
+      "https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-series-6-aluminum-silver-case-tachymeter-watchface_09152020_inline.jpg.large.jpg",
+    },
+  },
+  {
+    name: "Samsung Galaxy S21",
+    description: "Samsung Galaxy S21",
+    price: 799.99,
+    url: {
+      black: "https://images.samsung.com/n_africa/smartphones/galaxy-s21/buy/s21_ultra_phantomblack_colorselection_720x720_img.jpg",
+    },
+  },
+  {
+    name: "Playstation 5",
+    description: "Sony Playstation 5",
+    price: 499.99,
+    url: {
+      white: "https://m.media-amazon.com/images/I/619BkvKW35L._AC_SX466_.jpg",
+    },
+  },
+  {
+    name: "Xbox Series X",
+    description: "Microsoft Xbox Series X",
+    price: 499.99,
+    url: {
+      black:
+        "https://www.xtremeskins.co.uk/cdn/shop/products/xbox-series-x-black-leather-skins_2048x.jpg?v=1714722137",
+      white:
+        "https://metro.co.uk/wp-content/uploads/2024/02/White-Xbox-Series-X-2b57.jpg?quality=90&strip=all",
+    },
+  },
+  {
+    name: "Nintendo Switch",
+    description: "Nintendo Switch",
+    price: 299.99,
+    url: {
+      classic:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Nintendo-Switch-wJoyCons-BlRd-Standing-FL.jpg/2560px-Nintendo-Switch-wJoyCons-BlRd-Standing-FL.jpg",
+      gold: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/My%20Nintendo%20Store/EN-US/Hardware/nintendo-switch-lite-hyrule-edition-12-months-nso-121164/121164-121169-nintendo-switch-lite-hyrule-edition-front-1200x675",
+      black:
+        "https://i5.walmartimages.com/asr/8fbef61a-a4fe-4363-8653-6de9fff5804a.38d8d3d29efb91dca63eba02014a61e8.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+    },
+  },
+  {
+    name: "Dell XPS 13",
+    description: "Dell XPS 13",
+    price: 999.99,
+    url: {
+      black: "https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/xps-notebooks/xps-13-9310/general/xps-13_black_keyboard-view.jpg?fmt=pjpg&pscan=auto&scl=1&wid=3278&hei=2403&qlt=100,1&resMode=sharp2&size=3278,2403&chrss=full&imwidth=5000",
+    },
+  },
+];
